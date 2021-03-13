d93cffeb1163def74b7a0a03338e752744fd68a8
wangyao <3050740551@qq.com>
2021-03-13 12:55:18 +0800

    springboot启动器

---
+++ src/main/java/com/wy/plan/PlanApplication.java
@@ -0,0 +1,17 @@
+package com.wy.plan;
+
+import com.wy.plan.pojo.Thing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class PlanApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PlanApplication.class, args);
+
+    }
+}
